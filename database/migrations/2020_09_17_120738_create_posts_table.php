3b990a71b081a03ad68620a9fb6fd0161d23aaff
hasetai13 <ttaisei0103@gmail.com>
2020-09-17 12:11:44 +0900

    define table

---
+++ database/migrations/2020_09_17_120738_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('image_path');
+            $table->string('title');
+            $table->string('content');
+            $table->string('category');
+            $table->string('unit_status');
+            $table->string('prefecture');
+            $table->string('area');
+            $table->string('gender');
+            $table->string('age');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
