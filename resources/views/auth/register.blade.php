0080a4841dcaff1a5b777f39e0ab1a479691a8b2
hasetai13 <ttaisei0103@gmail.com>
2020-11-07 18:21:42 +0900

    deleate modal

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -64,32 +64,32 @@
                         <div class="form-group row mb-0">
                             <!-- Button trigger modal -->
                             <div class="col-md-6 offset-md-4">
-                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                <button type="submit" class="btn btn-primary">
                                     {{ __('確認する') }}
                                 </button>
                                 <a href="/">トップに戻る</a>
                             </div>
 
                             <!-- Modal -->
-                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLabel">こちらの内容で登録しますか？</h5>
-                                            <button type="submit" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            ...
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">戻る</button>
-                                            <button type="button" class="btn btn-primary">登録する</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+{{--                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">--}}
+{{--                                <div class="modal-dialog" role="document">--}}
+{{--                                    <div class="modal-content">--}}
+{{--                                        <div class="modal-header">--}}
+{{--                                            <h5 class="modal-title" id="exampleModalLabel">こちらの内容で登録しますか？</h5>--}}
+{{--                                            <button type="submit" class="close" data-dismiss="modal" aria-label="Close">--}}
+{{--                                                <span aria-hidden="true">&times;</span>--}}
+{{--                                            </button>--}}
+{{--                                        </div>--}}
+{{--                                        <div class="modal-body">--}}
+{{--                                            ...--}}
+{{--                                        </div>--}}
+{{--                                        <div class="modal-footer">--}}
+{{--                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">戻る</button>--}}
+{{--                                            <button type="button" class="btn btn-primary">登録する</button>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
                         </div>
                     </form>
                 </div>
