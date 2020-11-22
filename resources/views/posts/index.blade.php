75810e9860c4bca47f30a360599ec847d816307f
hasetai13 <ttaisei0103@gmail.com>
2020-11-22 11:00:08 +0900

    modify UI

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -66,10 +66,10 @@
                     <form action="/search/" method="get">
                         <div class="select-option mt-2 mb-4">
                             <div class="row mb-1">
-                                <div class="col-4 d-flex align-items-center">
+                                <div class="col-5 d-flex align-items-center">
                                     <h5><span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-3.148 0-6 2.553-6 5.702 0 4.682 4.783 5.177 6 12.298 1.217-7.121 6-7.616 6-12.298 0-3.149-2.851-5.702-6-5.702zm0 8c-1.105 0-2-.895-2-2s.895-2 2-2 2 .895 2 2-.895 2-2 2zm12 16l-6.707-2.427-5.293 2.427-5.581-2.427-6.419 2.427 4-9 3.96-1.584c.38.516.741 1.08 1.061 1.729l-3.523 1.41-1.725 3.88 2.672-1.01 1.506-2.687-.635 3.044 4.189 1.789.495-2.021.465 2.024 4.15-1.89-.618-3.033 1.572 2.896 2.732.989-1.739-3.978-3.581-1.415c.319-.65.681-1.215 1.062-1.731l4.021 1.588 3.936 9z"/></svg></span>都道府県</h5>
                                 </div>
-                                <div class="col-8">
+                                <div class="col-7">
                                     <select class="custom-select" name="prefecture">
                                         <option value="" selected>都道府県を選択</option>
                                         <option value="北海道">北海道</option>
@@ -123,10 +123,10 @@
                                 </div>
                             </div>
                             <div class="row mb-1">
-                                <div class="col-4 d-flex align-items-center">
+                                <div class="col-5 d-flex align-items-center">
                                     <h5><span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 7.001c0 3.865-3.134 7-7 7s-7-3.135-7-7c0-3.867 3.134-7.001 7-7.001s7 3.134 7 7.001zm-1.598 7.18c-1.506 1.137-3.374 1.82-5.402 1.82-2.03 0-3.899-.685-5.407-1.822-4.072 1.793-6.593 7.376-6.593 9.821h24c0-2.423-2.6-8.006-6.598-9.819z"/></svg></span>年齢</h5>
                                 </div>
-                                <div class="col-8">
+                                <div class="col-7">
                                     <select class="custom-select" name="age">
                                         <option selected value="">年齢を選択</option>
                                         <option value="10">10代</option>
@@ -136,10 +136,10 @@
                                 </div>
                             </div>
                             <div class="row mb-1">
-                                <div class="col-4 d-flex align-items-center">
+                                <div class="col-5 d-flex align-items-center">
                                     <h5><span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16 0v2h2.586l-2.113 2.113c-.981-.698-2.177-1.113-3.473-1.113-2.22 0-4.144 1.216-5.18 3.009-3.229.096-5.82 2.738-5.82 5.991 0 2.973 2.164 5.433 5 5.91v2.09h-3v2h3v2h2v-2h3v-2h-3v-2.09c1.791-.301 3.294-1.403 4.167-2.918 3.235-.09 5.833-2.735 5.833-5.992 0-1.296-.415-2.492-1.113-3.473l2.113-2.113v2.586h2v-6h-6zm-3 13c-1.944 0-3.564-1.396-3.923-3.236-.66-.333-1.365-.346-2.033-.066.266 2.293 1.827 4.181 3.931 4.938-.729.831-1.784 1.364-2.975 1.364-2.206 0-4-1.794-4-4s1.794-4 4-4c1.937 0 3.555 1.384 3.921 3.214.679.35 1.309.383 2.033.077-.27-2.293-1.837-4.179-3.943-4.931.732-.83 1.797-1.36 2.989-1.36 2.206 0 4 1.794 4 4s-1.794 4-4 4z"/></svg></span> 性別</h5>
                                 </div>
-                                <div class="col-8">
+                                <div class="col-7">
                                     <select class="custom-select" name="gender">
                                         <option selected value="">性別を選択</option>
                                         <option value="女性">女性</option>
@@ -160,8 +160,8 @@
 
                 {{--    ▼見出し　最近投稿されたコピユニ--}}
                 <div class="row">
-                    <h5 class="mt-5 mb-4 col-8"><span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 9.229c.234-1.12 1.547-6.229 5.382-6.229 2.22 0 4.618 1.551 4.618 5.003 0 3.907-3.627 8.47-10 12.629-6.373-4.159-10-8.722-10-12.629 0-3.484 2.369-5.005 4.577-5.005 3.923 0 5.145 5.126 5.423 6.231zm-12-1.226c0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-7.962-9.648-9.028-12-3.737-2.338-5.262-12-4.27-12 3.737z"/></svg></span> 最近投稿されたのコピユニ</h5>
-                    <div class="mt-5 mb-4 col-4 text-right" style="position: relative;">
+                    <h5 class="mt-5 mb-4 col-9"><span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 9.229c.234-1.12 1.547-6.229 5.382-6.229 2.22 0 4.618 1.551 4.618 5.003 0 3.907-3.627 8.47-10 12.629-6.373-4.159-10-8.722-10-12.629 0-3.484 2.369-5.005 4.577-5.005 3.923 0 5.145 5.126 5.423 6.231zm-12-1.226c0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-7.962-9.648-9.028-12-3.737-2.338-5.262-12-4.27-12 3.737z"/></svg></span> 最近投稿されたコピユニ</h5>
+                    <div class="mt-5 mb-4 col-3 text-right" style="position: relative;">
                         <p style="position: absolute; right: 10px; bottom: -20px;"><a href="/search">もっと見る</a></p>
                     </div>
                 </div>
@@ -200,15 +200,11 @@
                                                     <img class="card-img-top" src="uploads/{{ $post -> image_path }}" alt="Card image cap">
                                                     <div class="card-body">
                                                         <p class="card-title">{{ $post -> title }}</p>
-                                                        <div>
-                                                            <span href="#" class="card-link">{{ $post -> prefecture }}</span>
-                                                        </div>
-                                                        <div>
-                                                            <span href="#" class="card-link">{{ $post -> age . '代' }}</span>
-                                                        </div>
-                                                        <div>
-                                                            <span href="#" class="card-link">{{ $post -> gender }}</span>
-                                                        </div>
+                                                        <ul class="card-lists">
+                                                            <li class="card-list">{{ $post -> prefecture }}</li>
+                                                            <li class="card-list">{{ $post -> age . '代' }}</li>
+                                                            <li class="card-list">{{ $post -> gender }}</li>
+                                                        </ul>
                                                     </div>
                                                 </div>
                                             </a>
